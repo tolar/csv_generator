4304b749de34b20083e2c3d1c9c9a9172e9f8757
Vaclav Tolar <Vaclav Tolar>
2011-07-26 01:52:38 +0200

    project setup

--- csv_generator/app/controllers/Application.java
+++ csv_generator/app/controllers/Application.java
@@ -8,8 +8,12 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
-
+	
     public static void index() {
+        render();
+    }
+
+    public static void step1() {
         render();
     }
 
