b0d0ab072780a01e6dbd19e7bec79ba482fc4129
Vaclav Tolar <Vaclav Tolar>
2011-09-01 22:56:52 +0200

    value length validation

--- csv_generator/app/models/Step2Params.java
+++ csv_generator/app/models/Step2Params.java
@@ -1,5 +1,6 @@
 package models;
 
+import net.sf.oval.constraint.Length;
 import play.data.validation.Max;
 import play.data.validation.Min;
 import play.data.validation.Required;
@@ -7,6 +8,7 @@ import play.data.validation.Required;
 public class Step2Params {
 	
 	@Required(message="Hodnota buňky nesmí být prázdná")
+	@Length(max=20, message="Hodnota buňky nesmí být delší než 20 znaků")
 	public String cellValue;
 
 	@Override
