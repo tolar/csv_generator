c963da8b4cc90a034fb3ac375c20d3a2ee8da144
Vaclav Tolar <Vaclav Tolar>
2011-10-12 21:59:01 +0200

    localilaztion step3 and step4

--- csv_generator/conf/messages.cs
+++ csv_generator/conf/messages.cs
@@ -5,9 +5,27 @@ login=Přihlásit
 feedback=Navrhněte vylepšení!
 start=Start
 next=Další
+previous=Previous
+add=Přidat
+remove=Odstranit
 headline=Generátor CSV souborů
 form_error_message=Opravte, prosím, následující chyby ve formuláři
+delimiter=Oddělovač
+file_name=Jméno souboru
+generate=Generovat
+
 step1_title=Generátor CSV souborů - krok 1
 step1_headline=Krok 1. - definování dimenzí
 step1_row_count=Počet řádků
 step1_column_count=Počet sloupců
+
+step2_title=Generátor CSV souborů - krok 2
+step2_headline=Krok 2. - definování hodnot
+step2_new_value=Nová hodnota pro buňky
+
+step3_title=Generátor CSV souborů - krok 3
+step3_headline=Krok 3. - sestavení souboru 
+
+step4_title=Generátor CSV souborů - krok 4
+step4_headline=Krok 4. - dokončení
+ 
