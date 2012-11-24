1f36e06d164e29d6bb317a3f63f015cdfaa64d93
Vaclav Tolar <vaclav.tolar@gmail.com>
2012-11-25 00:09:51 +0100

    New Registration controller

--- csv_generator2/app/controllers/Application.scala
+++ csv_generator2/app/controllers/Application.scala
@@ -25,50 +25,6 @@ object Application extends Controller {
     }
     
 
-    case class User(
-        username: String, 
-        email: String,
-        password: String, 
-        passwordConfirm: String)
-    
-    val registrationForm = Form[User] (
-        mapping (
-            "username" -> text(minLength = 6),
-            "email" -> email,
-            "password" -> tuple (
-                "main" -> text(minLength = 6),
-                "again" -> text(minLength = 6)
-            ).verifying (
-                Messages("password_not_same"), passwords => passwords._1 == passwords._2)            
-        ) 
-        
-        {
-          (username, email, passwords) => User(username, email, passwords._1, "")
-        }
-        {
-          user => Some(user.username, user.email, (user.password, ""))
-        }.verifying(
-            Messages("username_already_exists"), 
-            user => DAO.findUserByUsername(user.username).isEmpty
-        )
-       
-    )
-    
-    def registration = Action {
-      Ok(views.html.registration(registrationForm));
-    }
-    
-    def register = Action { implicit request => 
-      	registrationForm.bindFromRequest.fold(
-      			errors => {      				
-      				BadRequest(views.html.registration(errors)) 
-      			},
-      			user => {
-      			  DAO.insertUser(user.username, user.email, DigestUtils.md5Hex(user.password))
-      			  Redirect(routes.Application.index("registration_successfull"))
-      			}
-    	)
-    }      
     
     
         
