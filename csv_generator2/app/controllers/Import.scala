212c8b583cc29d686887ea8942422cc39daf33e7
Vaclav Tolar <Vaclav Tolar>
2012-12-06 23:05:03 +0100

    Importing file

--- csv_generator2/app/controllers/Import.scala
+++ csv_generator2/app/controllers/Import.scala
@@ -16,6 +16,8 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import scala.io.Source
+import scala.collection.SortedSet
 
 object Import extends Controller {
   
@@ -39,13 +41,35 @@ object Import extends Controller {
     Ok(views.html.importFile(importForm))
   }  
   
-  def processImport = Action(parse.multipartFormData) { request =>
+  def processImport = Action(parse.multipartFormData) { implicit request =>
     request.body.file("filePath").map { csvFile =>
       import java.io.File
       val filename = csvFile.filename 
       val contentType = csvFile.contentType
-      csvFile.ref.moveTo(new File("/tmp/picture"))
-      Ok("File uploaded")
+      csvFile.ref.moveTo(new File("/tmp/csv.csv"), true)
+      
+      var values = Set[String]()
+      var rowNo = 0
+      var columnNo = 0
+      
+      for(line <- Source.fromFile("/tmp/csv.csv").getLines()) {
+        println(line)
+        values ++= (line.split(";")).toSet // TODO delimiter from form 
+        rowNo += 1
+        columnNo = if (values.size > columnNo) values.size else columnNo
+      }
+      
+      val gs = controllers.Application.getSessionValue(session)
+      
+      gs.cellValues ++= values
+      gs.rowsNo = rowNo
+      gs.columnsNo = columnNo
+      
+      controllers.Application.updateSessionValue(gs, session)
+      
+      Ok(views.html.index()).flashing("successKey" -> "import_successful")
+    	 
+      
     }.getOrElse {
       Redirect(routes.Application.index).flashing(
     		  "error" -> "Missing file")
